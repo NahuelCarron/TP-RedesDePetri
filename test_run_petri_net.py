b1a1208c0c03c6ce735ba8ef9a183977d82a82a6
Nahuel <80365484+NahuelCarron@users.noreply.github.com>
2022-10-26 17:26:19 -0300

    primer push

---
+++ test_run_petri_net.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+#
+# Tests for run_petri_net.py module
+#
+
+
+"""Tests for run_petri_net.py module"""
+
+
+# Standard packages
+## NOTE: this is empty for now
+
+# Installed packages
+import pytest
+
+# Local packages
+## NOTE: this is empty for now
+
+
+class TestRunPetriNet:
+    """Tests class for run_petri_net"""
+
+    ## TODO: WORK-IN-PROGRESS
+
+
+if __name__ == '__main__':
+    pytest.main([__file__])
